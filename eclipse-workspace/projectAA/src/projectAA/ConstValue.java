605873f3bff61be36daa8001e72edff8b81f3f5b
jmcaboy <benedict0421@gmail.com>
2021-04-22 17:55:06 +0900

    (wish)final commit

---
+++ eclipse-workspace/projectAA/src/projectAA/ConstValue.java
@@ -0,0 +1,26 @@
+package projectAA;
+
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class ConstValue {
+	static Scanner scanner = new Scanner(System.in);
+	static int a, b;	//입력받을 값
+	static double result, result1, change;	//계산 결과값
+	static int change1;		//거스름돈 소수점 자른 값
+	final static int [] dollars = {100,50,20,10,5,2,1};	//달러(유로)로 내줄 값
+	final static int [] kowon = {1000,500,100,50,10};	//원화(엔화)로 내줄 값
+	final static double [] ex_rate = {1133.40, 1349.23, 1019.95};	//환율
+	final static String [] country = {"USD","EUR","JPY"};	//입력받을 환전국가
+	static ArrayList<Integer>results = new ArrayList<Integer>();	//환전 결과 저장용
+	static ArrayList<Integer>changes = new ArrayList<Integer>();	//거스름돈 결과 저장용
+	static ArrayList<String>output = new ArrayList<String>();		//파일에 넣을 결과 저장용
+	static Calendar cal = Calendar.getInstance();	//날짜 출력
+	static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");	//날짜 출력 형태 지정
+	static String date = df.format(cal.getTime());	//날짜 출력 형태로 입력한 문자열
+	static FileWriter fw;	//파일에 쓰기 위해 만들었음
+	static boolean isFileExist;		//파일이 있는지 없는지 판단
+}
---
+++ eclipse-workspace/projectAA/src/projectAA/Main.java
@@ -0,0 +1,53 @@
+package projectAA;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.TimeZone;
+
+
+
+public class Main extends InputMoney {
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Main main = new Main();
+		while(true) {
+			main.Menu();
+		}
+		
+	}//main
+		
+	
+	
+}	
+
+
+
+
+
