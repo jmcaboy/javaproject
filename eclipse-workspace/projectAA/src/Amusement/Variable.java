03aadf7745266b62cb734516bf7b9960d570c75c
jmcaboy <61782508+jmcaboy@users.noreply.github.com>
2021-04-28 16:00:39 +0900

    Delete eclipse-workspace/projectAA/src/Amusement directory

--- eclipse-workspace/projectAA/src/Amusement/Variable.java
+++
@@ -1,9 +0,0 @@
-package Amusement;
-
-public class Variable extends Constant{
-	static int will;
-	static String ID = Input.InputAge();
-	static int age = Processing.CalAge(ID, currentYear);
-	static double Discount_rate = Input.Disc();
-	static int price = Processing.Choice_Ticket(age);
-}
